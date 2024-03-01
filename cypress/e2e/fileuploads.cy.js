b9dc2453f07587aec67d1652f7bc43da5576caaa
Temiloluwa Sanyaolu <stemtol@gmail.com>
2024-03-01 17:24:27 +0100

    upload complete

--- cypress/e2e/fileuploads.cy.js
+++ cypress/e2e/fileuploads.cy.js
@@ -6,6 +6,26 @@ describe('fileuploads', () => {
         cy.get("#file-submit").click()
         cy.wait(5000)
         cy.get("div[class='example'] h3").should('have.text',"File Uploaded!")
-
     })
+
+    it('Rename', () => {
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        cy.get("#file-upload ").attachFile({filePath:'Screenshot 2024-02-01 at 15.13.40.png',fileName:'new.png'})
+        cy.get("#file-submit").click()
+        cy.wait(5000)
+        cy.get("div[class='example'] h3").should('have.text',"File Uploaded!")
+    })
+
+    it.only('drag and drop', () =>{
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        cy.get("#drag-drop-upload").attachFile('draganddrop.png',{subjectType:'drag-n-drop'})
+        // for multiple files just include them in attachment, separate with a comma
+        cy.wait(5000)
+    })
+
+    it.skip('shadow dom element', () => {
+        cy.visit('https://www.htmlelements.com/demos/fileupload/shadow-dom/index.htm')
+        cy.get('.smart-browse-input',{includeShadowDom:true}).attachFile('draganddrop.png')
+
+    });
 })
